c0781d6a41542457cce737c3d923e5bc1ade31e1
Angel Chung <achung998@gmail.com>
2026-07-26 17:09:48 -0400

    Add: majority-element-ii - submission-4

---
+++ Data Structures & Algorithms/majority-element-ii/submission-4.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    majorityElement(nums) {
+        let a = -1;
+        let b = -1;
+
+        let countA = 0;
+        let countB = 0;
+
+        for(const num of nums) {
+            if(num === a) {
+                ++countA;
+            } else if(num === b) {
+                ++countB;
+            } else if(countA === 0) {
+                a = num;
+                ++countA;
+            } else if(countB === 0) {
+                b = num;
+                ++countB;
+            } else {
+                --countA;
+                --countB;
+            }
+        }
+
+        const res = [];
+        const third = Math.floor(nums.length/3);
+        countA = 0;
+        countB = 0;
+
+        for(const num of nums) {
+            if(num === a) ++countA;
+            else if(num === b) ++countB;
+        }
+
+        if(countA > third) res.push(a);
+        if(countB > third) res.push(b);
+
+        return res;
+    }
+}
